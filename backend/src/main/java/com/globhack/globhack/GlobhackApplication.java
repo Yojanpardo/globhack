33e7a9c59b05f3ba27d6421af1c24b396e21b3ac
Yojan Pardo <yojan.pardo@gmail.com>
2020-05-30 08:50:02 -0500

    base del backend

---
+++ backend/src/main/java/com/globhack/globhack/GlobhackApplication.java
@@ -0,0 +1,13 @@
+package com.globhack.globhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobhackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/globhack/globhack/GlobhackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globhack.globhack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobhackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
