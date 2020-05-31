fb4e05404ce182d3d06fddd78cf957a8de9bb20f
joelsantz <joelsantzdev@gmail.com>
2020-05-31 08:22:43 -0500

    add sigin component

--- frontend/globhack/src/App.js
+++ frontend/globhack/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Blog from './components/Home/Blog';
+import SignIn from './components/SignIn/SignInSide';
 
 const App = () => (
     <Router>
         <Route path = "/" exact  component = {Blog} />
+        <Route path = "/signin" exact component = {SignIn}/>
     </Router>
 );
 
