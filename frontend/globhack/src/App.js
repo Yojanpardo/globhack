47e70fa2bcd73be511f004630c430dbd0a2d3c6c
joelsantz <joelsantzdev@gmail.com>
2020-05-31 08:37:01 -0500

    add signup component

--- frontend/globhack/src/App.js
+++ frontend/globhack/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Blog from './components/Home/Blog';
 import SignIn from './components/SignIn/SignInSide';
+import SignUp from './components/SignUp/SignUp';
 
 const App = () => (
     <Router>
         <Route path = "/" exact  component = {Blog} />
         <Route path = "/signin" exact component = {SignIn}/>
+        <Route path = "/signup" exact component = {SignUp}/>
     </Router>
 );
 
